f7129858d6b9f651097790f973a8183bc354bb9d
George MacKerron <george@mackerron.co.uk>
2020-12-28 17:23:37 +0000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'json-stringify-pretty-compact' {
+declare module '@aitodotai/json-stringify-pretty-compact' {
   const stringify: (object: any, options?: {
       indent?: number | string,
       maxLength?: number,
